9df504deb3c42761d094cd015eeb677a19babd9d
brambooo <bram_reinold@hotmail.com>
2016-08-08 19:30:24 +0200

    Modified Skills section, is working now!

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,11 +1,17 @@
-$( document ).ready(function() {
-    console.log( "ready!" );
+$(document).ready(function() {
+
+    console.log('test');
 
     // Skills section - animate all skill bars
     // Loop through all skill-bar divs
-    $('#skills .skills').each(function(){
-        $(this).find('.skill-bar').animate({
-            width:jQuery(this).attr('data-percent')
+
+    $('.skill-bar').each(function(){
+        var iWidth = $(this).attr('data-percent');
+        console.log(iWidth);
+
+        $(this).find('.skill-bar-loadingbar').animate({
+            width:iWidth
         }, 6000);
     });
-});
+
+}); // end ready
