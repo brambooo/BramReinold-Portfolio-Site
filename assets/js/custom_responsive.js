474202f33bff0c3b531611809ebe2f31090ad3ca
brambooo <bram_reinold@hotmail.com>
2016-08-14 15:16:06 +0200

    Refactored mobile nav js

--- assets/js/custom_responsive.js
+++ assets/js/custom_responsive.js
@@ -5,38 +5,33 @@
 $(document).ready(function () {
     var closeBtnDiv = $('.mobile-menu-close-btn');
     var openBtnDiv = $('.mobile-menu-open-btn');
-    var menuDesktopDiv = $('#main-menu');
     var menuMobileDiv = $('#main-menu-mobile');
 
-    //closeBtnDiv.hide();     // hide close btn
+    // Default menu is hidden (closed)
     menuMobileDiv.hide();     // hide main menu
 
     // Event handler to open
     openBtnDiv.on('click', function() {
         // hide open div
         $(this).hide();
-        console.log('open menu');
+        closeBtnDiv.show();
         // open mobile menu
-        menuMobileDiv.slideToggle('slow', function() {
-            closeBtnDiv.show();      // open close div
-            console.log('closebtn show');
-        });
+        menuMobileDiv.slideToggle('slow');
     });
 
     // Event handler to close
     closeBtnDiv.on('click', function() {
-        // close mobile menu
-        console.log('sluit menu');
-        menuMobileDiv.slideToggle('slow', function(){
-            closeBtnDiv.hide();     // close closeBtnDiv
-            openBtnDiv.show();      // open openBtnDiv
-            console.log('openBtn show');
-        });
+        closeBtnDiv.hide();     // close closeBtnDiv
+        openBtnDiv.show();      // open openBtnDiv
+        menuMobileDiv.slideToggle('slow');
     });
 
     // Event that checks if there is clicked on any nav ul li item, if so close toggle
     $('nav ul li').on('click', function(){
+        // Close menu
         menuMobileDiv.hide();
+        closeBtnDiv.hide();
+        openBtnDiv.show();
     });
 
 });
